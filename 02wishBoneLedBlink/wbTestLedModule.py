b0dbfe45370db2f577fd855b420c34122a986fd2
Rune Langøy <rune.langoy@gmail.com>
2026-02-17 11:23:05 +0100

    added test for more invalid rages

--- 02wishBoneLedBlink/wbTestLedModule.py
+++ 02wishBoneLedBlink/wbTestLedModule.py
@@ -33,7 +33,27 @@ while(True):
 
    ####
 
-   addr=0x20000000
+   addr=0x40000004
+   print(f" Write 0x01 to address 0x{addr:08x}  — No device attetched noting should happen - please verify")
+   wb.write(addr, 0x1)
+
+   # Read back to verify
+   value = wb.read(addr)
+   print(f"LED register = 0x{value:08x} (bit0={'ON' if value & 1 else 'OFF'})")
+
+   input("Press Enter to continue...")
+   print(f" Write 0x00 to address 0x{addr:08x}  — No device attetched noting should happen - please verify")
+   wb.write(addr, 0x0)
+
+   # Read back to verify
+   value = wb.read(addr)
+   print(f"LED register = 0x{value:08x} (bit0={'ON' if value & 1 else 'OFF'})")
+
+   input("Press Enter to continue...")
+
+  ####
+
+   addr=0x50000000
    print(f" Write 0x01 to address 0x{addr:08x}  — No device attetched noting should happen - please verify")
    wb.write(addr, 0x1)
 
